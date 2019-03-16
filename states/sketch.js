f255b5c6b75f43f75bc2280125795a889039eb00
Ehsan <ehsansoltan21@gmail.com>
2019-03-16 16:42:25 -0700

    Update sketch.js

--- states/sketch.js
+++ states/sketch.js
@@ -21,6 +21,10 @@ class character{
     this.time = 0;
     this.timeUntilLast = 0;
     this.timeFromLast = 0;
+
+    this.maxFrame = 9;
+
+    this.state = "stationary";
   }
 
 
@@ -53,15 +57,64 @@ class character{
       }
     
     //resetting the stationary animation after the ninth frame
-    if (this.currentFrameNum > 9){ 
+    
+    if (this.currentFrameNum > this.maxFrame){ 
       this.currentFrameX = 0;
       this.currentFrameNum = 0;
     }
+  
+
+  }
+
+  rightState(){
+    this.state = "right";
+    this.currentFrameY = 436.
+    this.currentFrameX = 0;
+    this.frameHeight = 114;
+    this.frameWidth = 130;
+    this.maxFrame = 9;
+    }
+
+
+  stationaryState(){
+    this.state = "stationary";
+    this.currentFrameX = 0;
+    this.currentFrameY = 0;
+    this.frameHeight = 112;
+    this.frameWidth = 79;
+    this.maxFrame = 9;
+  }
+
+  //movement
+  move(){
+    if (keyIsDown(RIGHT_ARROW)){
+      
+    }
+    else {
+     
+
+      }
+  }
+
+
+
+
+}
+
+
+
+function keyPressed(){
+  if (keyCode === RIGHT_ARROW){
+    char1.rightState();
 
   }
 }
 
-
+function keyReleased(){
+  if (keyCode === RIGHT_ARROW){
+    char1.stationaryState();
+  }
+}
 
 
 let char1 = new character();
@@ -73,6 +126,7 @@ function setup() {
 
 function draw() {
   background(220);
+  char1.move();
   char1.draw();
   char1.getTime();
   char1.animate();
