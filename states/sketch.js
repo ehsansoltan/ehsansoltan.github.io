cead0eb5728fbbc6b6469bf37f261e984c391c46
Ehsan <ehsansoltan21@gmail.com>
2019-03-15 20:16:07 -0700

    stationary animation
    got the stationary animation working

--- states/sketch.js
+++ states/sketch.js
@@ -9,24 +9,56 @@
 
 class character{
   constructor(){
+
+    //defining some variables
     this.charX = 200;
     this.frameHeight = 112;
     this.frameWidth = 79;
     this.currentFrameX = 0;
     this.currentFrameY = 0;
     this.spriteSheet;
-    this.currentFrame = 1;
+    this.currentFrameNum = 1;
+    this.time = 0;
+    this.timeUntilLast = 0;
+    this.timeFromLast = 0;
   }
 
+
+  //loading all the sprites i need from a single file
   loadSpriteSheet(){
       this.spriteSheet = loadImage('assets/sprites.jpg');
   }
+  
+  //function for drawing a certain portion from the spritesheet
   draw(){
       image(this.spriteSheet, 0, 0, 118.5, 168, this.currentFrameX, this.currentFrameY, this.frameWidth, this.frameHeight);
   }
 
+  //using 'millis()' to get the time and the time from the last animation frame switch
+  getTime(){
+    this.time = millis();
+    this.timeFromLast = this.time - this.timeUntilLast;
+  }
+
+  //changing animation frames every 55 milliseconds
   animate(){
+    if (this.timeFromLast > 55){
+        this.currentFrameNum++;
+        this.currentFrameX += this.frameWidth;
+        this.timeFromLast = 0;
+        this.timeUntilLast = millis();
+      }
     
+    //resetting the stationary animation after the ninth frame
+    if (this.currentFrameNum > 9){ 
+      this.currentFrameX = 0;
+      this.currentFrameNum = 0;
+    }
+    
+   
+      
+
+  
 
   }
 }
@@ -43,11 +75,12 @@ function setup() {
 function draw() {
   background(220);
   char1.draw();
-  setInterval(function(){
-      char1.currentFrameX += char1.frameWidth;
-      char1.currentFrame++;
-      if (char1.currentFrame === frameWidth*)
+  char1.getTime();
+  char1.animate();
+    
 
-  }, 300);
+
+  
+  
  
 }
