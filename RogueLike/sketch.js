09e022a1e18a130b15b0e5281475e49adcb2ac32
ehsansoltan <47368897+ehsansoltan@users.noreply.github.com>
2019-05-01 15:19:58 -0600

    added corridors
    I made an algorithm which adds corridors which branches out from the "seed room"

--- RogueLike/sketch.js
+++ RogueLike/sketch.js
@@ -10,12 +10,24 @@ class map{
   constructor(){
     this.map = [];
     this.mapSize = 50;
-    this.mapTileSize = 10;
+    this.mapTileSize = 13;
     this.textSize = 25;
 
     this.currentRoomWidth;
     this.currentRoomHeight;
     this.maxRoomSize = 10;
+
+    this.corridorX;
+    this.corridorY;
+    this.corridorDirection;
+    this.corridorLength;
+    this.roomEdge = false;
+    this.corridorXChange = 0;
+    this.corridorYChange = 0;
+    this.maxCorridorLength = 25;
+    this.minCorridorLength = 15;
+    
+
 
   }
 
@@ -42,6 +54,72 @@ class map{
     }
   }
 
+  //picks a random corridor spot in which a corridor will branch out from
+  findCorridorSpot(){
+
+    this.corridorXChange = 0;
+    this.corridorYChange = 0;
+    this.roomEdge = false;
+
+    while (this.roomEdge === false){
+      this.corridorX = Math.floor(random(1, this.mapSize - 1));
+      this.corridorY = Math.floor(random(1, this.mapSize - 1));
+
+      if (this.map[this.corridorY][this.corridorX] === "#"){
+
+        if (this.map[this.corridorY + 1][this.corridorX] === "."){
+          this.corridorDirection = "up";
+          this.corridorYChange = -1;
+          this.roomEdge = true;
+        }
+
+        else if (this.map[this.corridorY - 1][this.corridorX] === "."){
+          this.corridorDirection = "down";
+          this.corridorYChange = 1;
+          this.roomEdge = true;
+        }
+
+        else if (this.map[this.corridorY][this.corridorX + 1] === "."){
+          this.corridorDirection = "left";
+          this.corridorXChange = -1;
+          this.roomEdge = true;
+        }
+
+        else if (this.map[this.corridorY][this.corridorX - 1] === "."){
+          this.corridorDirection = "right";
+          this.corridorXChange = 1;
+          this.roomEdge = true;
+        }
+
+
+      }
+    }
+  }
+
+
+  addCorridor(){
+    this.corridorLength = random(this.minCorridorLength, this.maxCorridorLength);
+
+    while (this.corridorLength > 0 && this.corridorX + this.corridorXChange > 0 && this.corridorX + this.corridorXChange < this.mapSize && this.corridorY + this.corridorYChange > 0 && this.corridorY + this.corridorYChange < this.mapSize && this.corridorLength < this.maxCorridorLength){
+
+      //end corridor if it intersects an existing corridor
+      if (this.map[this.corridorY][this.corridorX] === ".") this.corridorLength = 0;
+      
+      this.map[this.corridorY][this.corridorX] = ".";
+      this.corridorY += this.corridorYChange;
+      this.corridorX += this.corridorXChange;
+      this.corridorLength--;
+    }
+
+  }
+
+  addCorridors(iterations){
+    for (let i = 0; i < iterations; i++){
+      this.findCorridorSpot();
+      this.addCorridor();
+    }
+
+  }
 
   //draws the map
   drawMap(size){
@@ -63,11 +141,13 @@ function setup() {
   map1 = new map();
   map1.createEmptyMap(map1.mapSize);
   map1.placeSeedRoom(25, 25);
+  map1.addCorridors(10);
 }
 
 function draw() {
   background(220);
  
   map1.drawMap(map1.mapSize);
+  
 
 }
